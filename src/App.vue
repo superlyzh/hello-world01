8ae40eceb14609a8b1169a80d7a9e8845c3a7467
e <l>
2021-03-02 18:35:18 +0800

    半自动部署测试

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
       <p>java0821 全体同学 前程似锦</p>
       <p>java0821 全体同学 身体健康</p>
+     <p>java0821 全体同学 年入三十万</p>
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
 </template>
